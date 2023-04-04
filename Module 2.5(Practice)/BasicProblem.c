49956b1ed0db8e291911144c441a0a52d5db2987
RD RAYHAN <rdrayhan7916@gmail.com>
2023-04-05 00:39:00 +0600

    comment out

--- Module 2.5(Practice)/BasicProblem.c
+++ Module 2.5(Practice)/BasicProblem.c
@@ -1,75 +1,75 @@
     // Problem-01
 
-// #include <stdio.h>
-// int main (){
-//     printf("Recently I heard that you’ve achieved 95%% marks in your exam. This is brilliant! I wish you’ll shine in your life!	Good luck with all the barriers(//\\) in your life.");
-//     return 0;
-// }
+#include <stdio.h>
+int main (){
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam. This is brilliant! I wish you’ll shine in your life!	Good luck with all the barriers(//\\) in your life.");
+    return 0;
+}
 
 
   // Problem-02
 
-//   #include <stdio.h>
-//   int main(){
-//     int a , b, sum, subtraction, multipliacation;
-//     float divition;
-//     scanf("%d, %d", &a, &b);
-//     sum= a+b;
-//     subtraction = a-b;
-//     multipliacation =a*b;
-//     divition = a*1.0/b;
-//     printf("Sum=%d\n Subtraction=%d\n Multiplication=%d\n Division=%0.2f" , sum, subtraction, multipliacation, divition);
-//   }
+  #include <stdio.h>
+  int main(){
+    int a , b, sum, subtraction, multipliacation;
+    float divition;
+    scanf("%d, %d", &a, &b);
+    sum= a+b;
+    subtraction = a-b;
+    multipliacation =a*b;
+    divition = a*1.0/b;
+    printf("Sum=%d\n Subtraction=%d\n Multiplication=%d\n Division=%0.2f" , sum, subtraction, multipliacation, divition);
+  }
 
    // Problem-03
 
-//    #include <stdio.h>
-//    int main (){
-//     int a;
-//     scanf("%d", &a);
-//     if (a > 0)
-//     {
-//         if (a %2==0)
-//         {
-//             printf("This is even number");
-//         }
-//         else{
-//             printf("This is a odd number");
-//         }
+   #include <stdio.h>
+   int main (){
+    int a;
+    scanf("%d", &a);
+    if (a > 0)
+    {
+        if (a %2==0)
+        {
+            printf("This is even number");
+        }
+        else{
+            printf("This is a odd number");
+        }
         
-//     }
-//     else
-//     {
-//         printf("Please Give a Positive number");
-//     }
+    }
+    else
+    {
+        printf("Please Give a Positive number");
+    }
     
     
-//    }
+   }
 
 
 
 //    Problem-04
 
-// #include <stdio.h>
-// int main ()
-// {
-//     int a ;
-//     scanf("%d", &a);
-//     if (a== 0)
-//     {
-//     printf("This is a zero");
-//     }
-//     else if (0< a)
-//     {
-//         printf("This is a positive number");
+#include <stdio.h>
+int main ()
+{
+    int a ;
+    scanf("%d", &a);
+    if (a== 0)
+    {
+    printf("This is a zero");
+    }
+    else if (0< a)
+    {
+        printf("This is a positive number");
 
-//     }
-//    else
-//    {
-//     printf("This is a negetive number");
-//    }
+    }
+   else
+   {
+    printf("This is a negetive number");
+   }
   
-// }
+}
 
 // Problem-05
 
